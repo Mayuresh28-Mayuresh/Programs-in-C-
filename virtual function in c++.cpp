9ae10fc512dc5a6353ed17a603a3bee88795a61f
Mayuresh28-Mayuresh <111062567+Mayuresh28-Mayuresh@users.noreply.github.com>
2023-01-17 23:00:25 +0530

    Virtual Function in C++
    Virtual Function in C++

---
+++ virtual function in c++.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+
+using namespace std;
+
+class base
+ {
+ 	 public :
+ 	 	void display()
+ 	 	 {
+ 	 	 	 cout << "\nDisplay of Base";
+		   }
+		   
+		virtual void show()
+		 {
+		 	 cout << "\nShow of Base";
+		 }
+ };
+ 
+ 
+ 
+ class derived : public base
+  {
+  	   public :
+ 	 	void display()
+ 	 	 {
+ 	 	 	 cout << "\nDisplay of Derived";
+		   }
+		   
+	 void show()
+		 {
+		 	 cout << "\nShow of Derived";
+		 }
+  };
+
+
+
+
+
+
+
+
+
+
+
+int main()
+
+ {
+ 	
+	 base b_obj;
+	 derived d_obj;
+	 
+	 base *bptr;
+	 
+	 bptr = &b_obj;
+	 
+	 bptr -> display();
+	 bptr -> show();
+	 
+	 cout <<endl;
+	 
+	 
+	 bptr = &d_obj;
+	 
+	 bptr -> display();
+	 bptr -> show();
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 return 0;
+ }
